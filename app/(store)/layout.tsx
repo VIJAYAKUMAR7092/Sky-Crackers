acacf054590f01ac4ff74a31e30b5b252e35bdea
Vijaya Kumar <vijayakumar709202@gmail.com>
2026-09-23 14:31:44 +0530

    Fix prisma OS mismatch by building on AWS with optimizations

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React from "react";
 import Navbar from "../../components/public/layout/Navbar";
 import Footer from "../../components/public/layout/Footer";
